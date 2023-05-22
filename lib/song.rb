75c125a55e6247b9c0e54f9193d52d1c942fdc0f
John <johnkaranjairungu@gmail.com>
2023-05-22 17:18:32 +0300

    added instance methods

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@song_count = 0
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@song_count += 1
+    end
+    def self.song_count
+        @@song_count
+    end
+end
